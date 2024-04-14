7cbfc9988bc3fefdfbfcb1a52e47964f6ecbdce0
leeson <97571961+leesonaa@users.noreply.github.com>
2024-04-14 16:27:22 +0800

    fix: fix sth bug

--- debian_init.sh
+++ debian_init.sh
@@ -3,17 +3,18 @@
 ipaddr=$(ip a | grep /24 | awk '{print $2}' | awk -F "/" '{print $1}')
 appdata_path=""
 compose_path=""
-cd /tmp
 mainmenu() {
 
 	echo -e " \033[33;1;5m<欢迎使用 debian12一键初始化建议脚本>\033[0m"
-	echo " ----------------------------"
+	echo " --------------------------------------"
 	echo "        \   ^__^                       "
 	echo "         \  (oo)\_______               "
 	echo "            (__)\ leeson)\/            "
 	echo "                ||----w |              "
 	echo "                ||     ||              "
-	echo -e " 1 \033[36m更换国内中科大源\033[0m"
+	echo " --------------------------------------"
+	repo_changed=$(check_repo_changed)
+	echo -e " 1 \033[36m更换国内中科大源 — $repo_changed\033[0m"
 	echo -e " 2 \033[36m安装实用工具\033[0m"
 	webmin_installed=$(check_webmin_installed)
 	echo -e " 3 \033[32m安装webmin — $webmin_installed\033[0m"
@@ -31,59 +32,72 @@ mainmenu() {
 		echo "退出脚本"
 		exit 0
 	elif [ "$num" = 1 ]; then
-		echo "正在更换国内中科大源..."
 		change_repo
-		mainmenu
+		
 	elif [ "$num" = 2 ]; then
-		echo "正在安装实用工具..."
+		
 		install_tool
-		mainmenu
+		
 	elif [ "$num" = 3 ]; then
 		install_webmin
-		mainmenu
+		
 	elif [ "$num" = 4 ]; then
 		install_docker
-		mainmenu
+		
 	elif [ "$num" = 5 ]; then
-		echo "正在安装上面所有内容..."
+		
 		onekey
-		mainmenu
+		
 	elif [ "$num" = 6 ]; then
-		echo "正在安装dockge管理面板..."
+		
 		install_dockge
-		mainmenu
+		
 	else
 		echo -e "\033[31m请输入正确的数字！\033[0m"
-		mainmenu
+		
 	fi
+	mainmenu
 	
 }
 
 check_dockge_installed() {
-    if docker ps -a --format '{{.Names}}' | grep -q '^dockge$'; then
-        echo -e "\033[32m[已安装]\033[0m"
+    if [ "$(check_docker_installed)" == "[已安装]" ] && docker ps -a --format '{{.Names}}' | grep -q '^dockge$'; then
+        echo -e "[已安装]"
     else
-        echo -e "\033[31m[未安装]\033[0m"
+        echo -e "[未安装]"
     fi
 }
 
 check_docker_installed() {
     if command -v docker >/dev/null 2>&1; then
-        echo -e "\033[32m[已安装]\033[0m"
+        echo -e "[已安装]"
     else
-        echo -e "\033[31m[未安装]\033[0m"
+        echo -e "[未安装]"
     fi
 }
 
 check_webmin_installed() {
     if command -v webmin >/dev/null 2>&1; then
-        echo -e "\033[32m[已安装]\033[0m"
+        echo -e "[已安装]"
     else
-        echo -e "\033[31m[未安装]\033[0m"
+        echo -e "[未安装]"
     fi
 }
 
+check_repo_changed(){
+	if grep -q "ustc" /etc/apt/sources.list; then
+	    echo "[已更换]"   
+	else
+	    echo "[未更换]"
+	fi	
+}
+
+
 change_repo() {
+	if [ "$(check_repo_changed)" == "[已更换]" ]; then
+		echo "你已经换过了..."
+	else
+	echo "正在更换源地址..."
 	mv /etc/apt/sources.list /etc/apt/sources.list.bak
 	cat <<EOF >/etc/apt/sources.list
 deb https://mirrors.ustc.edu.cn/debian/ bookworm main contrib non-free non-free-firmware
@@ -91,38 +105,53 @@ deb https://mirrors.ustc.edu.cn/debian/ bookworm-updates main contrib non-free n
 deb https://mirrors.ustc.edu.cn/debian/ bookworm-backports main contrib non-free non-free-firmware
 deb https://mirrors.ustc.edu.cn/debian-security/ bookworm-security main contrib non-free non-free-firmware
 EOF
-	echo -e " \033[32m 中科大源替换完成\033[0m"
+	echo -e " \033[32m 中科大源替换完成,正在更新系统...\033[0m"
+	apt update && apt upgrade -y && apt install nala
+	fi
 }
 
 install_tool() {
-	apt update && apt install nala -y
-	nala update && nala install -y curl duf btop
-	echo -e " \033[32m您的系统挂载点信息如下\033[0m"
-	duf
+
+	if [ "$(check_repo_changed)" == "[已更换]" ]; then
+		echo "正在安装实用工具..."
+		nala update && nala upgrade -y && nala install -y curl duf btop 
+		echo -e " \033[32m您的系统挂载点信息如下\033[0m"
+		duf
+	else
+	  echo "请先替换源..."
+	fi
 }
 
 install_webmin() {
-	apt update && apt install nala -y
-	wget https://mirror.ghproxy.com/https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
-	sh setup-repos.sh <<<"y"
-	nala install -y --install-recommends webmin
-	echo -e " \033[32mwebmin安装成功，请用\033[0m\033[33;1;5mhttps://$ipaddr:10000\033[0m\033[32m访问后台\033[0m"
-	echo -e " \033[32m账户密码为服务器ssh的账户密码\033[0m"
+	if [ "$(check_webmin_installed)" == "[已安装]" ]; then
+		echo "你已经安装过了..."
+	elif [ "$(check_repo_changed)" == "[已更换]" ]; then
+		wget https://mirror.ghproxy.com/https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
+		sh setup-repos.sh <<<"y"
+		nala install -y --install-recommends webmin
+		echo -e " \033[32mwebmin安装成功，请用\033[0m\033[33;1;5mhttps://$ipaddr:10000\033[0m\033[32m访问后台\033[0m"
+		echo -e " \033[32m账户密码为服务器ssh的账户密码\033[0m"
+	else
+		echo "请先替换源..."
+		
+	fi
 }
 
 install_docker() {
-	apt update && apt install nala -y
-	if command -v curl >/dev/null 2>&1; then
-		echo "正在准备安装docker环境..."
+	if [ "$(check_docker_installed)" == "[已安装]" ]; then
+		echo "你已经安装过了..."
+	elif [ "$(check_repo_changed)" == "[已更换]" ]; then
+		echo "正在准备安装docker服务"	
+		nala update && nala install -y docker.io docker-compose
+		echo "docker服务安装已经完成"
 	else
-		echo "正在安装curl && docker && docker-compose"
-		nala update && nala install -y curl
+		echo "请先替换源..."
 	fi
-	curl -fsSL https://get.docker.com | sh
-	nala install -y docker-compose
+
 }
 
 onekey() {
+	echo "准备更改repo,安装工具,安装docker服务,安装dockge容器..."
 	change_repo
 	install_tool
 	install_docker
@@ -131,7 +160,6 @@ onekey() {
 
 check_path_exist() {
 	local path="$1"
-
 	if [ -e "$path" ]; then
 		return 0 # 路径存在返回 0 true
 	else
@@ -140,7 +168,10 @@ check_path_exist() {
 }
 
 install_dockge() {
-	if command -v docker >/dev/null 2>&1; then
+	if [ "$(check_dockge_installed)" == "[已安装]" ]; then
+		echo "dockge服务已经运行了..."
+	elif [ "$(check_docker_installed)" == "[已安装]" ]; then
+		echo "正在安装dockge管理面板..."
 		while true; do
 			read -p "请输入容器配置存放路径（以/开始的绝对路径）: " path_app
 			if ! check_path_exist "$path_app"; then
@@ -162,10 +193,10 @@ install_dockge() {
 			fi
 		done
 		docker run -d --name dockge --restart unless-stopped -p 5001:5001 -v /var/run/docker.sock:/var/run/docker.sock -v "$appdata_path":/app/data -v "$compose_path":/opt/stacks -e DOCKGE_STACKS_DIR=/opt/stacks louislam/dockge	
-		if [[ $? == 0 ]]; then
+	    if [[ $? == 0 ]]; then
 			echo -e " \033[32mdockge安装成功，请用\033[0m\033[33;1;5mhttp://$ipaddr:5001\033[0m\033[32m访问后台\033[0m"
 			echo "================================================================="
-		else
+	    else
 			echo "请检查dockge是否运行成功"
 			docker ps -a | grep dockge
 		fi
